6bef19def1a94f3411e8feea86c385d97a893982
Stephan Seitz <stephan.seitz@fau.de>
2020-04-24 13:56:05 +0200

    Write draft of jit-execution

--- src/jit-execution.lisp
+++ src/jit-execution.lisp
@@ -1,7 +1,8 @@
 (defpackage petalisp-cuda.jitexecution
   (:use :petalisp-cuda.backend
-        :cl)
-  (:import-from :cl-cuda :void :set)
+        :petalisp.ir
+        :cl
+        :cl-cuda)
   (:import-from :cl-cuda.api.kernel-manager :*kernel-manager*
                                             :kernel-manager-define-function)
   (:import-from :petalisp.utilities :with-hash-table-memoization)
@@ -13,10 +14,10 @@
 (in-package petalisp-cuda.jitexecution)
 
 
-(defstruct jit-function ()
-  (kernel-symbol iteration-scheme))
+(defstruct (jit-function)
+  kernel-symbol iteration-scheme)
 
-(defgeneric generate-iteration-scheme ())
+(defgeneric generate-iteration-scheme (kernel backend))
 
 (defmethod generate-iteration-scheme (kernel backend)
   (make-block-iteration-scheme (petalisp.ir:kernel-iteration-space kernel)
@@ -44,41 +45,47 @@
           (make-jit-function :kernel-symbol function-name
                              :iteration-scheme iteration-scheme))))))
 
-(defun execute-kernel (kernel (backend cuda-backend))
+(defmethod execute-kernel (kernel (backend cuda-backend))
     (let ((compiled-function compile-kernel (kernel backend)))))
 
 (defun generate-kernel (kernel kernel-arguments buffers iteration-scheme)
   ;; Loop over domain
   (iteration-code iteration-scheme
-                  ;; kernel body
                   (let* ((instructions (petalisp.ir:map-instructions #'identity kernel))
-                        (buffer->kernel-argument (make-buffer->kernel-argument buffers kernel-arguments))
-                        (map-input (lambda (input) (map-input input buffer->kernel-argument))))
-                    (loop for instruction in (sort instructions #'< :key #'petalisp.ir:instruction-number)
-                          count t into i
-                          collect
-                          (let (($i (format-symbol "$~A" i)))
-                           (etypecase instruction
-                            (petalisp.ir:call-instruction
-                              `(,(map-call-operator (petalisp.ir:call-instruction-operator instruction))
-                                 ,@(petalisp.ir:map-instruction-inputs map-input instruction)))
-                            (petalisp.ir:iref-instruction
-                              (format nil "iref ~S~%"
-                                      (petalisp.ir:instruction-transformation instruction)))
-                            (petalisp.ir:load-instruction
-                              `(set 
-                                      (petalisp.type-inference:type-specifier
-                                        (buffer->kernel-argument (petalisp.ir:load-instruction-buffer instruction)))
-                                      (petalisp.ir:instruction-transformation instruction)))
-                            (petalisp.ir:store-instruction
-                              (format nil "store ~S ~S ~S~%"
-                                      (petalisp.type-inference:type-specifier
-                                        (petalisp.ir:buffer-ntype
-                                          (petalisp.ir:store-instruction-buffer instruction)))
-                                      (petalisp.ir:instruction-transformation instruction)
-                                      (simplify-input
-                                        (first
-                                          (petalisp.ir:instruction-inputs instruction)))))))))))
+                         (buffer->kernel-argument (make-buffer->kernel-argument buffers kernel-arguments))
+                         (map-input (lambda (input) (map-input input buffer->kernel-argument))))
+                    ;; kernel body
+                    (generate-instructions (sort instructions #'< :key #'petalisp.ir:instruction-number)
+                                          buffer->kernel-argument
+                                          map-input))))
+
+(defun generate-instructions (instructions map-input &optional (i 0))
+  (let (instruction (pop instructions))
+    ($i (format-symbol "$~A" i)))
+  `(let (($i ,(etypecase instruction
+                (petalisp.ir:call-instruction
+                  `(set (#|c-type|#     ,(cl-cuda-type-from-buffer (petalisp.ir:load-instruction-buffer instruction))
+                         #|identifier|# $i
+                         #|value|#      
+                         `(,(map-call-operator (petalisp.ir:call-instruction-operator instruction))
+                            ,@(petalisp.ir:map-instruction-inputs map-input instruction)))
+                        (petalisp.ir:iref-instruction
+                          (format nil "iref ~S~%"
+                                  (petalisp.ir:instruction-transformation instruction)))
+                        (petalisp.ir:load-instruction
+                          `(set (#|c-type|#     ,(cl-cuda-type-from-buffer (petalisp.ir:load-instruction-buffer instruction))
+                                 #|identifier|# $i
+                                 #|value|#      (petalisp.ir:instruction-transformation instruction))))
+                        (petalisp.ir:store-instruction
+                          (format nil "store ~S ~S ~S~%"
+                                  (petalisp.type-inference:type-specifier
+                                    (petalisp.ir:buffer-ntype
+                                      (petalisp.ir:store-instruction-buffer instruction)))
+                                  (petalisp.ir:instruction-transformation instruction)
+                                  (simplify-input
+                                    (first
+                                      (petalisp.ir:instruction-inputs instruction))))))))
+             ,(generate-instructions instruction map-input (1+ i))))))
 
 (defun map-call-operator (operator)
   operator)
