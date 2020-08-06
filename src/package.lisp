a48f5e9590c4777378306f594b2274d42b976f83
Stephan Seitz <stephan.seitz@fau.de>
2020-08-06 09:52:12 +0200

    Export function :reclaim-cuda-memory

--- src/package.lisp
+++ src/package.lisp
@@ -3,10 +3,13 @@
   (:use :cl
         :petalisp
         :iterate)
-  (:import-from :petalisp-cuda.backend :use-cuda-backend)
+  (:import-from :petalisp-cuda.backend :use-cuda-backend :cuda-memory-pool)
   (:import-from :petalisp-cuda.memory.cuda-array :make-cuda-array)
   (:export :use-cuda-backend
            :make-cuda-array
+           :reclaim-cuda-memory
            :*transfer-back-to-lisp*))
 (in-package :petalisp-cuda)
 
+(defun reclaim-cuda-memory ()
+  (petalisp-cuda.memory.memory-pool:reclaim-cuda-memory (cuda-memory-pool *backend*)))
