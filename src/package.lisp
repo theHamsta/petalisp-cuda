7318da59a63ae1b81eb46aa2d1960d5308c52719
Stephan Seitz <stephan.seitz@fau.de>
2020-08-06 10:39:46 +0200

    No longer export petalisp-cuda:make-cuda-array

--- src/package.lisp
+++ src/package.lisp
@@ -6,7 +6,6 @@
   (:import-from :petalisp-cuda.backend :use-cuda-backend :cuda-memory-pool :*transfer-back-to-lisp*)
   (:import-from :petalisp-cuda.memory.cuda-array :make-cuda-array)
   (:export :use-cuda-backend
-           :make-cuda-array
            :reclaim-cuda-memory
            :*transfer-back-to-lisp*))
 (in-package :petalisp-cuda)
