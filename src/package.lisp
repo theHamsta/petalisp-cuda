b568a812cc5cf46e6405d74f3ade7281e84598d4
Stephan Seitz <stephan.seitz@fau.de>
2020-08-06 10:37:03 +0200

    Export symbol petalisp-cuda:*transfer-back-to-lisp*

--- src/package.lisp
+++ src/package.lisp
@@ -3,7 +3,7 @@
   (:use :cl
         :petalisp
         :iterate)
-  (:import-from :petalisp-cuda.backend :use-cuda-backend :cuda-memory-pool)
+  (:import-from :petalisp-cuda.backend :use-cuda-backend :cuda-memory-pool :*transfer-back-to-lisp*)
   (:import-from :petalisp-cuda.memory.cuda-array :make-cuda-array)
   (:export :use-cuda-backend
            :make-cuda-array
