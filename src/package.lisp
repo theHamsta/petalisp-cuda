b6ff53244b221eaef9a2ecc6580692e01d80ddfa
Stephan Seitz <stephan.seitz@fau.de>
2020-08-06 10:40:35 +0200

    Code formatting in package.lisp

--- src/package.lisp
+++ src/package.lisp
@@ -3,7 +3,9 @@
   (:use :cl
         :petalisp
         :iterate)
-  (:import-from :petalisp-cuda.backend :use-cuda-backend :cuda-memory-pool :*transfer-back-to-lisp*)
+  (:import-from :petalisp-cuda.backend :use-cuda-backend
+                                       :cuda-memory-pool
+                                       :*transfer-back-to-lisp*)
   (:import-from :petalisp-cuda.memory.cuda-array :make-cuda-array)
   (:export :use-cuda-backend
            :reclaim-cuda-memory
