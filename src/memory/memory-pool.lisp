e3d54bd34c5b714d7aec3cc15343922dda563b6f
Stephan Seitz <stephan.seitz@fau.de>
2020-08-21 22:01:10 +0200

    Lock memory on allocation/deallocations

--- src/memory/memory-pool.lisp
+++ src/memory/memory-pool.lisp
@@ -17,7 +17,9 @@
 
 (defclass cuda-memory-pool (memory-pool)
   ((%allocated-arrays :accessor allocated-cuda-arrays
-                      :initform '())))
+                      :initform '())
+   (%lock :accessor cuda-memory-pool-lock
+          :initform (bt:make-lock))))
 
 (defun make-cuda-memory-pool ()
   (make-instance 'cuda-memory-pool))
@@ -25,27 +27,30 @@
 (defmethod memory-pool-allocate ((memory-pool cuda-memory-pool)
                                  (array-element-type t)
                                  (array-dimensions number))
-  (let ((array (or (pop (gethash (cons array-element-type array-dimensions)
-                                 (array-table memory-pool)))(pop (gethash (cons array-element-type array-dimensions)
-                                 (array-table memory-pool)))
-                   (cl-cuda:alloc-memory-block array-element-type array-dimensions))))
-    (push array (allocated-cuda-arrays memory-pool))
-    array))
+  (bt:with-lock-held ((cuda-memory-pool-lock memory-pool))
+    (let ((array (or (pop (gethash (cons array-element-type array-dimensions)
+                                   (array-table memory-pool)))(pop (gethash (cons array-element-type array-dimensions)
+                                   (array-table memory-pool)))
+                     (cl-cuda:alloc-memory-block array-element-type array-dimensions))))
+      (push array (allocated-cuda-arrays memory-pool))
+      array)))
 
 (defmethod memory-pool-free ((memory-pool cuda-memory-pool)
                              (array cl-cuda.api.memory::memory-block))
-  (push array (gethash (cons (cl-cuda:memory-block-type array)
-                             (cl-cuda:memory-block-size array))
-                       (array-table memory-pool)))
-  (values))
+  (bt:with-lock-held ((cuda-memory-pool-lock memory-pool))
+    (push array (gethash (cons (cl-cuda:memory-block-type array)
+                               (cl-cuda:memory-block-size array))
+                         (array-table memory-pool)))
+    (values)))
 
 (defmethod memory-pool-free ((memory-pool cuda-memory-pool)
                              (array petalisp-cuda.memory.cuda-array:cuda-array))
    (memory-pool-free memory-pool (petalisp-cuda.memory.cuda-array:cuda-array-memory-block array)))
 
 (defmethod memory-pool-reset :before ((memory-pool cuda-memory-pool))
-  (mapcar #'cl-cuda:free-memory-block (allocated-cuda-arrays memory-pool))
-  (setf (allocated-cuda-arrays memory-pool) '()))
+  (bt:with-lock-held ((cuda-memory-pool-lock memory-pool))
+    (mapcar #'cl-cuda:free-memory-block (allocated-cuda-arrays memory-pool))
+    (setf (allocated-cuda-arrays memory-pool) '())))
 
 (defmethod reclaim-cuda-memory ((memory-pool cuda-memory-pool))
   (mapcar (lambda (array) (memory-pool-free memory-pool array)) (allocated-cuda-arrays memory-pool)))
