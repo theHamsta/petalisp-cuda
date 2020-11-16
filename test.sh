cba46e2a859a2fda41d2d5f07cc67de29fc85c3a
Stephan Seitz <stephan.seitz@fau.de>
2020-11-16 20:39:49 +0100

    Fail in test.sh

--- test.sh
+++ test.sh
@@ -7,4 +7,6 @@
 #
 
 
-sbcl --eval "(progn (ql:quickload :petalisp-cuda)(ql:quickload :petalisp-cuda/tests)(asdf:test-system :petalisp-cuda/tests))"
+sbcl --eval '(setf fiveam:*debug-on-error* t fiveam:*debug-on-failure* t)' \
+     --eval '(setf *debugger-hook* (lambda (c h) (declare (ignore c h)) (uiop:quit -1)))' \
+     --eval "(progn (ql:quickload :petalisp-cuda)(ql:quickload :petalisp-cuda/tests)(asdf:test-system :petalisp-cuda/tests))"
