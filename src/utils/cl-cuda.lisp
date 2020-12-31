147daf590c4e3e216c11b11c417a98da2178189a
Stephan Seitz <stephan.seitz@fau.de>
2020-12-31 15:34:22 +0100

    Fix create-corresponding-event

--- src/utils/cl-cuda.lisp
+++ src/utils/cl-cuda.lisp
@@ -27,8 +27,7 @@
   (cl-cuda.driver-api:cu-event-record cu-event cl-cuda:*cuda-stream*))
 
 (defun create-corresponding-event (thing &optional (event-map *cu-events*))
-  (let ((event (gethash thing event-map (create-cu-event))))
-    (setf (gethash thing event-map (create-cu-event)) event)))
+    (setf (gethash thing event-map) (create-cu-event)))
 
 (defun record-corresponding-event (thing &optional (event-map *cu-events*))
   (let ((event (gethash thing event-map)))
