ed7d9a0e2250bb6801907774ae866068937603a7
Stephan Seitz <stephan.seitz@fau.de>
2021-01-02 13:05:42 +0100

    Remove unnecessary (progn) in tests

--- tests/test-cuda-array.lisp
+++ tests/test-cuda-array.lisp
@@ -8,8 +8,7 @@
 (deftest test-aligned-allocation
   (let ((cl-cuda:*show-messages* nil))
     (cl-cuda:with-cuda (0)
-      (progn
-        (let* ((a (make-cuda-array '(20 9) 'float nil nil 32))
+      (let* ((a (make-cuda-array '(20 9) 'float nil nil 32))
                (b (make-cuda-array '(20 9) 'float))
                (c (make-cuda-array '(9 3) 'double nil nil 16)))
           (ok (equalp (cuda-array-strides a) '(32 1)))
@@ -20,21 +19,20 @@
           (ok (equalp (cuda-array-size c) (* 9 16)))
           (free-cuda-array a)
           (free-cuda-array b)
-          (free-cuda-array c))))))
+          (free-cuda-array c)))))
 
 (deftest half-array
   (if (boundp 'cl-cuda::+has-half-dtype+)
   (let ((cl-cuda:*show-messages* nil))
     (cl-cuda:with-cuda (0)
-      (progn
-        (let* ((a (make-cuda-array '(20 9) :half))
+      (let* ((a (make-cuda-array '(20 9) :half))
                (b (make-cuda-array '(9) :bfloat16)))
           (ok (equalp (cuda-array-strides a) '(10 1)))
           (ok (equalp (cuda-array-size a) 200))
           (ok (equalp (cuda-array-strides b) '(1)))
           (ok (equalp (cuda-array-size b) 10))
           (free-cuda-array a)
-          (free-cuda-array b))))))
+          (free-cuda-array b)))))
     (skip "No half support in cl-cuda (use theHamsta's fork)"))
 
 (deftest test-cuda-array-device
@@ -48,14 +46,13 @@
 (deftest test-mem-roundtrip
   (let ((cl-cuda:*show-messages* nil))
     (cl-cuda:with-cuda (0)
-      (progn
-        (let* ((foo (aops:rand* 'single-float '(20 9)))
+      (let* ((foo (aops:rand* 'single-float '(20 9)))
                (a (make-cuda-array foo 'float))
                (b (petalisp-cuda.memory.cuda-array:copy-cuda-array-to-lisp a)))
           (loop for i below (reduce #'* (array-dimensions foo))
                 do (progn
                      (assert (equal (row-major-aref foo i) (row-major-aref b i)))))
-                  (mapcar #'free-cuda-array (list a)))))))
+                  (mapcar #'free-cuda-array (list a))))))
 
 (deftest test-dont-allow-casts-in-strict-mode
   ;; should not work
