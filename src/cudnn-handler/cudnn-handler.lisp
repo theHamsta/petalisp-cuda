de8c2a2072b1a39f0cc61c8533cc08c40b264933
Stephan Seitz <stephan.seitz@fau.de>
2021-01-10 22:45:27 +0100

    Add support for forward convolution

--- src/cudnn-handler/cudnn-handler.lisp
+++ src/cudnn-handler/cudnn-handler.lisp
@@ -28,7 +28,7 @@
    (convolution-algorithms :accessor convolution-algorithms
                            :initform (make-hash-table :test #'equalp))
    (workspace :accessor workspace
-              :initform nil)
+              :initform 0)
    (workspace-size :accessor workspace-size
                    :initform 0)))
 
@@ -66,7 +66,7 @@
     (clear-foreign-hashtable (filter-descriptors cudnn-handler) #'cudnnDestroyFilterDescriptor)
     (alexandria:when-let ((workspace-mem (workspace cudnn-handler)))
       (cl-cuda:free-device-memory workspace-mem))
-    (setf (workspace cudnn-handler) nil)
+    (setf (workspace cudnn-handler) 0)
     (setf (workspace-size cudnn-handler) 0)
     (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnDestroy (cudnn-handle cudnn-handler))))))
 
@@ -100,15 +100,15 @@
 
 (defun cudnn-create-convolution-descriptor (input-array paddings dilations filter-strides mode cudnn-handler)
   (let ((input-type (cudnn-type (petalisp-cuda.memory.cuda-array::cuda-array-type input-array)))
-        (input-rank (length paddings)))
+        (array-length (length paddings)))
     (petalisp.utilities:with-hash-table-memoization
-      ((values input-array paddings dilations filter-strides mode))
+      ((values input-type paddings dilations filter-strides mode))
       (convolution-descriptors cudnn-handler)
       (with-foreign-objects ((descriptor '(:pointer cudnnConvolutionDescriptor-t))
-                             (padA :int 3)
-                             (dilA :int 3)
-                             (strideA :int 3))
-        (loop for i from 0 below input-rank
+                             (padA :int array-length)
+                             (dilA :int array-length)
+                             (strideA :int array-length))
+        (loop for i from 0 below array-length
               for p in paddings
               for d in dilations
               for s in filter-strides
@@ -119,10 +119,10 @@
                 (setf (mem-aref strideA :int i) s)))
         (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnCreateConvolutionDescriptor descriptor)))
         (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnSetConvolutionNdDescriptor (mem-aref descriptor 'cudnnConvolutionDescriptor-t)
-                                                                               input-rank
+                                                                               array-length
                                                                                padA
-                                                                               dilA
                                                                                strideA
+                                                                               dilA
                                                                                mode
                                                                                input-type)))
         (mem-aref descriptor 'cudnnConvolutionDescriptor-t)))))
@@ -148,22 +148,22 @@
 ;For N=4, a 4D filter descriptor, the filter layout is in the form of KRSC.
 ;For N=3, a 3D filter descriptor, the number S (number of columns per filter) is omitted and the layout of C immediately follows R.
 ;For N=5 and greater, the layout of the higher dimensions are inserted between S and C. For more information, see cudnnTensorFormat_t.
-(defun cudnn-create-filter-descriptor (input-array filter-array filter-format cudnn-handler)
-  (let ((input-type (cudnn-type (petalisp-cuda.memory.cuda-array::cuda-array-type input-array)))
+(defun cudnn-create-filter-descriptor (filter-array filter-format cudnn-handler)
+  (let ((input-type (cudnn-type (petalisp-cuda.memory.cuda-array::cuda-array-type filter-array)))
         (input-rank (rank filter-array)))
     (petalisp.utilities:with-hash-table-memoization
-      ((values input-array filter-array filter-format ))
+      ((values input-rank input-type filter-format (cuda-array-shape filter-array)))
       (convolution-descriptors cudnn-handler)
       (with-foreign-objects ((descriptor '(:pointer cudnnFilterDescriptor-t))
                              (filterDimA :int input-rank))
-        (loop for i from 0
+        (loop for i from 0 below input-rank
               for s in (cuda-array-shape filter-array)
               do (setf (mem-aref filterDimA :int i) s))
         (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnCreateFilterDescriptor descriptor)))
         (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnSetFilterNdDescriptor (mem-aref descriptor 'cudnnFilterDescriptor-t)
+                                                                          input-type
                                                                           filter-format
                                                                           input-rank
-                                                                          input-type
                                                                           filterDimA)))
         (mem-aref descriptor 'cudnnFilterDescriptor-t)))))
 
@@ -208,17 +208,15 @@
         (mem-ref new-descriptor :pointer))))
 
 (defun allocate-workspace (min-size cudnn-handler)
-  (progn
-    (when (<= (workspace-size cudnn-handler) min-size)
-      (progn
+    (when (< (workspace-size cudnn-handler) min-size)
         (when (workspace cudnn-handler)
           (cl-cuda:free-device-memory (workspace cudnn-handler)))
         (setf (workspace-size cudnn-handler) min-size)
         (setf (workspace cudnn-handler)
               (cl-cuda:alloc-device-memory 'cl-cuda:float
-                                           (/ (workspace-size cudnn-handler) 4)))))  ; allocate more than necessary ?
+                                           (/ (workspace-size cudnn-handler) 4))))  ; allocate more than necessary ?
     (values (workspace cudnn-handler)
-            (workspace-size cudnn-handler))))
+            (workspace-size cudnn-handler)))
 
 
 (defun cudnn-reduce-array (input-array
@@ -235,7 +233,7 @@
   (let ((input-descriptor (cudnn-create-tensor-descriptor input-array cudnn-handler))
         (output-descriptor (cudnn-create-tensor-descriptor output-array cudnn-handler))
         (reduction-descriptor (cudnn-create-reduction-descriptor reduce-op (cuda-array-type input-array) cudnn-handler))
-          (double-or-float (if (equalp (cuda-array-type input-array) :double) :double :float)))
+        (double-or-float (if (equalp (cuda-array-type input-array) :double) :double :float)))
       (with-foreign-objects ((workspace-min-size '(:pointer :int))
                                   (indices-min-size '(:pointer :int))
                                   (alpha '(:pointer :double))
@@ -281,8 +279,9 @@
     ((values input-descriptor filter-descriptor convolution-descriptor output-descriptor *cudnn-autotune*))
     (convolution-algorithms cudnn-handler)
     (with-foreign-object (algo-count '(:pointer :int))
-      (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnGetConvolutionForwardAlgorithmMaxCount (cudnn-handle cudnn-handler) algo-count)))
-      (with-foreign-object (perf-results '(:pointer cudnnConvolutionFwdAlgoPerf-t) (mem-aref algo-count :int))
+      (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnGetConvolutionForwardAlgorithmMaxCount (cudnn-handle cudnn-handler)
+                                                                                         algo-count)))
+      (with-foreign-object (perf-results 'cudnnConvolutionFwdAlgoPerf-t (mem-aref algo-count :int))
         (if *cudnn-autotune*
             ;cudnnStatus_t cudnnFindConvolutionForwardAlgorithm(
             ;cudnnHandle_t                      handle,
@@ -294,13 +293,13 @@
             ;int                               *returnedAlgoCount,
             ;cudnnConvolutionFwdAlgoPerf_t     *perfResults)
           (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnFindConvolutionForwardAlgorithm (cudnn-handle cudnn-handler)
-                                                             input-descriptor
-                                                             filter-descriptor
-                                                             convolution-descriptor
-                                                             output-descriptor
-                                                             (mem-aref algo-count :int)
-                                                             algo-count
-                                                             perf-results)))
+                                                                                      input-descriptor
+                                                                                      filter-descriptor
+                                                                                      convolution-descriptor
+                                                                                      output-descriptor
+                                                                                      (mem-aref algo-count :int)
+                                                                                      algo-count
+                                                                                      perf-results)))
           ;cudnnStatus_t cudnnGetConvolutionForwardAlgorithm_v7(
           ;cudnnHandle_t                       handle,
           ;const cudnnTensorDescriptor_t       xDesc,
@@ -320,6 +319,21 @@
                                                                                         perf-results))))
         (foreign-slot-value (mem-aref perf-results 'cudnnConvolutionFwdAlgoPerf-t) 'cudnnConvolutionFwdAlgoPerf-t 'algo)))))
 
+(defun check-output-dimensions (output-array input-descriptor convolution-descriptor filter-descriptor)
+  (with-foreign-objects ((result :int (rank output-array)))
+    (assert (equalp :CUDNN-STATUS-SUCCESS (cudnnGetConvolutionNdForwardOutputDim
+                                            convolution-descriptor
+                                            input-descriptor
+                                            filter-descriptor
+                                            (rank output-array)
+                                            result)))
+    (assert
+      (equalp
+        (cuda-array-shape output-array)
+        (loop for i from 0 below (rank output-array)
+              for s in (cuda-array-shape output-array)
+              collect (mem-aref result :int i))))))
+
 (defun cudnn-convolution (input-array
                            filter-array
                            output-array
@@ -331,56 +345,62 @@
                            (mode :cudnn-convolution)
                            bias
                            activation-function
-                           (paddings (make-list (rank input-array) :initial-element 0))
-                           (filter-strides (make-list (rank input-array) :initial-element 1))
-                           (dilations (make-list (rank input-array) :initial-element 1))
+                           (paddings (make-list (- (rank input-array) 2) :initial-element 1))
+                           (filter-strides (make-list (- (rank input-array) 2) :initial-element 1))
+                           (dilations (make-list (- (rank input-array) 2) :initial-element 1))
                            (filter-format :cudnn-tensor-nchw))
-  (declare (ignore algorithm bias activation-function))
+  (declare (ignore bias activation-function))
   (let* ((input-descriptor (cudnn-create-tensor-descriptor input-array cudnn-handler))
-        (output-descriptor (cudnn-create-tensor-descriptor output-array cudnn-handler))
-        (convolution-descriptor (cudnn-create-convolution-descriptor input-array paddings dilations filter-strides mode cudnn-handler))
-        (filter-descriptor (cudnn-create-filter-descriptor input-array filter-array filter-format cudnn-handler))
-        (double-or-float (if (equalp (cuda-array-type input-array) :double) :double :float))
-        (convolution-algorithm (get-convolution-forward-algorithm input-descriptor filter-descriptor convolution-descriptor output-descriptor cudnn-handler)))
+         (output-descriptor (cudnn-create-tensor-descriptor output-array cudnn-handler))
+         (convolution-descriptor (cudnn-create-convolution-descriptor input-array paddings dilations filter-strides mode cudnn-handler))
+         (filter-descriptor (cudnn-create-filter-descriptor filter-array filter-format cudnn-handler))
+         (double-or-float (if (equalp (cuda-array-type input-array) :double) :double :float))
+         (convolution-algorithm (progn
+                                  (check-output-dimensions output-array input-descriptor convolution-descriptor filter-descriptor)
+                                  (or algorithm (get-convolution-forward-algorithm input-descriptor
+                                                                                   filter-descriptor
+                                                                                   convolution-descriptor
+                                                                                   output-descriptor
+                                                                                   cudnn-handler)))))
 
     (with-foreign-objects ((workspace-min-size '(:pointer :int))
-                                (alpha '(:pointer :double))
-                                (beta '(:pointer :double)))
-          ; this routine supports mixing data types, then alpha, beta are float or else everything is double
-          (setf (mem-ref alpha double-or-float) (convert-to-foreign input-factor double-or-float))
-          (setf (mem-ref beta double-or-float) (convert-to-foreign accumulator-factor double-or-float))
-          (assert (equalp :CUDNN-STATUS-SUCCESS
-                          (cudnnGetConvolutionForwardWorkspaceSize (cudnn-handle cudnn-handler)
-                                                                   input-descriptor
-                                                                   filter-descriptor
-                                                                   convolution-descriptor
-                                                                   output-descriptor
-                                                                   convolution-algorithm
-                                                                   workspace-min-size)))
-          (multiple-value-bind (workspace workspace-size) (allocate-workspace (mem-ref workspace-min-size :int) cudnn-handler)
-            (assert (equalp :CUDNN-STATUS-SUCCESS
-                            (cudnnConvolutionForward (cudnn-handle cudnn-handler)
-                                                     alpha ; &alpha == (type) 1 <- /* Tensor operation : C = reduce op( alpha * A ) + beta * C */
-                                                     input-descriptor
-                                                     (make-pointer (device-ptr input-array))
-                                                     filter-descriptor
-                                                     (make-pointer (device-ptr filter-array))
-                                                     convolution-descriptor
-                                                     convolution-algorithm
-                                                     (make-pointer workspace)
-                                                     workspace-size
-                                                     beta ; &beta = (type) 0 <- /* Tensor operation : C = reduce op( alpha * A ) + beta * C */
-                                                     output-descriptor
-                                                     (make-pointer (device-ptr output-array)))))))
-        ;cudnnStatus_t cudnnGetConvolutionForwardWorkspaceSize(
-        ;cudnnHandle_t   handle,
-        ;const   cudnnTensorDescriptor_t         xDesc,
-        ;const   cudnnFilterDescriptor_t         wDesc,
-        ;const   cudnnConvolutionDescriptor_t    convDesc,
-        ;const   cudnnTensorDescriptor_t         yDesc,
-        ;cudnnConvolutionFwdAlgo_t               algo,
-        ;size_t                                 *sizeInBytes)
-        ))
+                           (alpha '(:pointer :double))
+                           (beta '(:pointer :double)))
+      ; this routine supports mixing data types, then alpha, beta are float or else everything is double
+      (setf (mem-ref alpha double-or-float) (convert-to-foreign input-factor double-or-float))
+      (setf (mem-ref beta double-or-float) (convert-to-foreign accumulator-factor double-or-float))
+      (assert (equalp :CUDNN-STATUS-SUCCESS
+                      (cudnnGetConvolutionForwardWorkspaceSize (cudnn-handle cudnn-handler)
+                                                               input-descriptor
+                                                               filter-descriptor
+                                                               convolution-descriptor
+                                                               output-descriptor
+                                                               convolution-algorithm
+                                                               workspace-min-size)))
+      (multiple-value-bind (workspace workspace-size) (allocate-workspace (mem-ref workspace-min-size :int) cudnn-handler)
+        (assert (equalp :CUDNN-STATUS-SUCCESS
+                        (cudnnConvolutionForward (cudnn-handle cudnn-handler)
+                                                 alpha ; &alpha == (type) 1 <- /* Tensor operation : C = reduce op( alpha * A ) + beta * C */
+                                                 input-descriptor
+                                                 (make-pointer (device-ptr input-array))
+                                                 filter-descriptor
+                                                 (make-pointer (device-ptr filter-array))
+                                                 convolution-descriptor
+                                                 convolution-algorithm
+                                                 (make-pointer workspace)
+                                                 workspace-size
+                                                 beta ; &beta = (type) 0 <- /* Tensor operation : C = reduce op( alpha * A ) + beta * C */
+                                                 output-descriptor
+                                                 (make-pointer (device-ptr output-array)))))))
+    ;cudnnStatus_t cudnnGetConvolutionForwardWorkspaceSize(
+    ;cudnnHandle_t   handle,
+    ;const   cudnnTensorDescriptor_t         xDesc,
+    ;const   cudnnFilterDescriptor_t         wDesc,
+    ;const   cudnnConvolutionDescriptor_t    convDesc,
+    ;const   cudnnTensorDescriptor_t         yDesc,
+    ;cudnnConvolutionFwdAlgo_t               algo,
+    ;size_t                                 *sizeInBytes)
+    ))
 
 "
 cudnnStatus_t cudnnConvolutionForward(
