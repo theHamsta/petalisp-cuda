10abd079a56018eed1bcbd084d9483978783e068
Stephan Seitz <stephan.seitz@fau.de>
2021-02-23 17:37:28 +0100

    Add test for unnormalizing-transformation

--- tests/test-custom-op.lisp
+++ tests/test-custom-op.lisp
@@ -30,3 +30,27 @@
                                             ;(2 2))))
                                      ;#4A((((2))))))
         )
+
+(deftest unnormalizing-transformation
+  (ok
+    (equalp
+      (~ 1 ~ 7 ~ 4)
+      (let ((normalized (transform
+                          (~ 1 ~ 2 9 ~ 3 7)
+                          (normalizing-transformation (~ 1 ~ 2 9 ~ 3 7)))))
+        (transform
+          normalized
+          (petalisp-cuda.cudnn-ops::unnormalizing-transformation
+            normalized
+            (~ 1 ~ 2 9 ~ 3 7))))))
+  (ok
+    (equalp
+      (~ 1 ~ 7 ~ 4)
+      (let ((collapsed (transform
+                          (~ 1 ~ 2 9 ~ 3 7)
+                          (collapsing-transformation (~ 1 ~ 2 9 ~ 3 7)))))
+        (transform
+          collapsed
+          (petalisp-cuda.cudnn-ops::unnormalizing-transformation
+            collapsed
+            (~ 1 ~ 2 9 ~ 3 7)))))))
